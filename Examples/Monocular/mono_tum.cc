3af7acf4252cbe0655c2bab4218cd7e90f4d8fcd
tyuownu <tyuownu@gmail.com>
2018-12-19 19:46:38 +0800

    (docs): force argc = 5(mono_tum)

--- Examples/Monocular/mono_tum.cc
+++ Examples/Monocular/mono_tum.cc
@@ -36,7 +36,7 @@ void LoadImages(const string &strFile, vector<string> &vstrImageFilenames,
 
 int main(int argc, char **argv)
 {
-    if(argc != 4 && argc != 5)
+    if(argc != 5)
     {
         cerr << endl << "Usage: ./mono_tum path_to_vocabulary path_to_settings path_to_sequence [whether_show_viewer]" << endl;
         return 1;
