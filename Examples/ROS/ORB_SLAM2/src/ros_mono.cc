5820f3ea068d204bc25ce98f13d6ae2aa82aa56c
tyuownu <tyuownu@gmail.com>
2018-12-26 10:04:05 +0800

    (docs): adding new feature(save/load map) for ros node

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -50,15 +50,15 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "Mono");
     ros::start();
 
-    if(argc != 3 && argc != 4)
+    if(argc != 5)
     {
-        cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings [whether_show_viewer]" << endl;
+        cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings [whether_show_viewer] [whether_save_map]" << endl;
         ros::shutdown();
         return 1;
     }    
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,strcmp(argv[3], "false") ? true : false);
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,strcmp(argv[3], "false") ? true : false, bool(atoi(argv[4])));
 
     ImageGrabber igb(&SLAM);
 
